7678b15a944ecb321c82c4a41f5c059938e9cfef
Ganesh Kalyanshetti <ganeshkalyanshetti7@gmail.com>
2025-11-28 18:02:37 +0530

    Implement totalsum function for counting subsets
    Refactor totalsum function to calculate target sum count.

--- Classic/Sums-subset2.py
+++ Classic/Sums-subset2.py
@@ -56,3 +56,33 @@ n=3
 totalsum(0,a,res,givenarr,target,sums,n)
 
 print(res)
+
+
+# if u want count
+def totalsum(i,a,res,givenarr,target,sums,n):
+    if i==n:
+        return 0
+        
+    a.append(givenarr[i])
+    sums+=givenarr[i]
+    
+    if target==sums:
+        return 1
+            
+    l=totalsum(i+1,a,res,givenarr,target,sums,n)
+    
+    sums-=givenarr[i]
+    a.pop()
+    
+    r=totalsum(i+1,a,res,givenarr,target,sums,n)
+    
+    return l+r
+a=[]
+res=[]
+givenarr=[1,2,1]
+sums=0
+target=2
+n=3
+result = totalsum(0,a,res,givenarr,target,sums,n)
+
+print(result)
